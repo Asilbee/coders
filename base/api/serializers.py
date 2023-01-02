d09854909b65c7ffb49da3fee947af987f229ce4
Asilbee <asilweb9999@gmail.com>
2023-01-02 13:01:50 +0500

    first commit

---
+++ base/api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework.serializers import ModelSerializer
+from base.models import User, Room, Topic, Message
+
+
+class RoomSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+class TopicSerializer(ModelSerializer):
+    class Meta:
+        model = Topic
+        fields = '__all__'
+
+class MessageSerializer(ModelSerializer):
+    class Meta:
+        model = Message
+        fields = '__all__'
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
